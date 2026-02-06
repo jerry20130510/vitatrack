d697a04371d174a21fb624f05146b401ef73e870
Sam <britpop1992@gmail.com>
2026-02-07 00:03:00 +0800

    fix: 修正會員資料、隱私設定及我的訂單的連結

--- src/main/webapp/member/js/memberCenter.js
+++ src/main/webapp/member/js/memberCenter.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const contentArea = document.getElementById('content-area');
     const menuLinks = document.querySelectorAll('.menu-link');
     const profile = menuLinks[0];
-    
+
 
     profile.addEventListener("click", function (e) {
         e.preventDefault();
@@ -23,18 +23,18 @@ document.addEventListener("DOMContentLoaded", function () {
                                 <div class="member-form">
                                     <div class="form-row"><label>使用者帳號</label><p class="readonly">${member.email}</p></div>
                                     <div class="form-row"><label>姓名</label><p class="readonly">${member.name}</p>
-                                        <button class="mn-btn-1 btn" id="nameBtn"type="button" ><span>編輯</span></button>
+                                        <button class="mn-btn-1" id="nameBtn"type="button" ><span>編輯</span></button>
                                     </div>
 
                                     <div class="form-row"><label>Email</label><p class="readonly">${member.email}</p></div>
                                     <div class="form-row"><label>密碼</label><p class="readonly">${member.password}</p>
-                                        <button class="mn-btn-1 btn" id="passwordBtn"type="button"><span>編輯</span></button>
+                                        <button class="mn-btn-1 " id="passwordBtn"type="button"><span>編輯</span></button>
                                     </div>
                                     <div class="form-row"><label>地址</label><p class="readonly">${member.address}</p>
-                                        <button class="mn-btn-1 btn" id="addressBtn"type="button"><span>編輯</span></button>
+                                        <button class="mn-btn-1 " id="addressBtn"type="button"><span>編輯</span></button>
                                     </div>
                                     <div class="form-row"><label>手機號碼</label><p class="readonly">${member.phone}</p>
-                                      <button class="mn-btn-1 btn" id="phoneBtn"type="button"><span>編輯</span></button>
+                                      <button class="mn-btn-1 " id="phoneBtn"type="button"><span>編輯</span></button>
                                     </div>
                                 </div>
                                 
@@ -47,52 +47,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
     });
 
-    const nameBtn = document.getElementById('nameBtn');
-    const emailBtn = document.getElementById('emailBtn');
-    const passwordBtn = document.getElementById('passwordBtn');
-    const addressBtn = document.getElementById('addressBtn');
+    contentArea.addEventListener('click', function (e) {
+        //點擊按鈕要跳出輸入框
+        //找到編輯按鈕
+        const btn = e.target.closest('.mn-btn-1');
+        console.log(btn);
+        if (!btn) return;
 
-
-    nameBtn.addEventListener('click',function(){
-    //點擊按鈕要跳出姓名輸入框
-     fetch('profile') // 你的 Servlet
-            .then(res => res.json())
-            .then(member => {
-         contentArea.innerHTML = `
-                        <div class="member-card">
-                            <header class="member-card-header">會員資料</header>
-                            <div class="member-card-body">
-                                <div class="member-form">
-                                    <div class="form-row"><label>姓名</label><input type="text">${member.name}</div>
-                                        <button class="mn-btn-1 btn" id="nameBtn"type="button" ><span>儲存</span></button>
-                                        <button class="mn-btn-1 btn" id="nameBtn"type="button" ><span>取消</span></button>
-                                    </div>
-                                </div>
-                                
-                            </div>
-                        </div>`;
-            });
-    
-    });
-
-
-
-    
-    emailBtn.addEventListener('click',function(){
-
+        //找到與按鈕同層級的p標籤
+        const p = btn.parentElement.querySelector('.readonly');
+        console.log(p);
         
+
+
+       
     });
 
-    passwordBtn.addEventListener('click',function(){
 
-        
-    });
 
-    addressBtn.addEventListener('click',function(){
 
-        
-    });
 
-    
 
 });
